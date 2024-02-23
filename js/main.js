fd2292d7d2b2b7ae7b234ac0eafd8bec83c9b082
bearm12 <bearm12493@gmail.com>
2024-02-23 10:22:19 +0000

    winky face

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 'use strict';
+const face = document.querySelector('.js-face');
+const head = document.querySelector('.js-head');
+
+
+// function winkFace(){
+//     face.innerHTML = ';)';
+// }
+
+function normalFace (){
+    face.innerHTML = ':)';
+}
+head.addEventListener('click', (){
+    face.innerHTML = ';)';
+});
+head.addEventListener('mouseout', normalFace);
